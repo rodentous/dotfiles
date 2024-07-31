5d48b45354f8aca532949869c43b40dafa03a101
rodentous <therodentous@gmail.com>
2024-07-31 14:07:57 +0300

    Update config/dotfiles.toml Update config/shell/preferences.sh Update .gitconfig

--- config/shell/preferences.sh
+++ config/shell/preferences.sh
@@ -7,26 +7,29 @@ prompt()
 }
 PROMPT_COMMAND="prompt"
 
+
 ### COMPLETION ########################################################################################################################################################################################
-bind TAB:menu-complete # cycle options on tab, not just show them
-bind '"\e[Z": menu-complete-backward' # and shift+tab cycle backwards
-bind '"\C-h": backward-kill-word' # ctrl-backspace removes word
-bind "set show-all-if-ambiguous on" # display a list of the matching files
-# perform partial (common) completion on the first tab press, only start
-# cycling full results on the second Tab press (from bash version 5):
+bind TAB:menu-complete                      # cycle options on tab, not just show them
+bind "'\e[Z': menu-complete-backward"       # and shift+tab cycle backwards
+bind "set show-all-if-ambiguous on"         # display a list of the matching files
 bind "set menu-complete-display-prefix on"
-# set completion-ignore-case On
+bind "set completion-ignore-case on"
+
+bind "'\C-h': backward-kill-word"           # ctrl+backspace removes word
+
 
 ### NERD FONT #########################################################################################################################################################################################
 export NERD_FONT=1
+
 
 ### TRUE COLOR ########################################################################################################################################################################################
 export COLORTERM=truecolor
 export MICRO_TRUECOLOR=1
 
+
 ### HISTORY ###########################################################################################################################################################################################
-export HISTCONTROL=ignoreboth:erasedups      # no duplicates in history
-shopt -s histverify                          # "!command" will be editable
-# INFINITE history:
-export HISTSIZE= 
-export HISTFILESIZE=
+export HISTCONTROL=ignoreboth:erasedups     # delete duplicates
+shopt -s histverify                         # "!command" will be editable
+
+export HISTSIZE=                            # I N F I N I T E
+export HISTFILESIZE=                        # H I S T O R Y
